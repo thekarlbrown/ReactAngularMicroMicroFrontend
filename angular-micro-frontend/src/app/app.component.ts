3662d49434bae88a938de040f02de537785792c2
Karl Brown <karlbrownthinks@gmail.com>
2024-02-24 02:01:32 -0500

    react and angular microfrontends complete

--- angular-micro-frontend/src/app/app.component.ts
+++ angular-micro-frontend/src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-micro-frontend';
+  public loadingReact = false;
+  public reactLoaded = false;
+  constructor() {
+  }
+
+  launchReactMicroFrontend(): void {
+    this.loadingReact = true;
+
+    const script = document.createElement('script');
+  script.src = 'https://angularreactmicromicrofrontend.s3.amazonaws.com/reactMicro.js'; // Ensure this URL is correct
+  
+  script.onload = () => {
+    this.loadingReact = false;
+    this.reactLoaded = true;
+  };
+  script.onerror = (error) => {
+    console.error('Error loading React micro frontend script:', error);
+  };
+  document.body.appendChild(script);
+  }
 }
--- react-micro-frontend/src/App.js
+++ react-micro-frontend/src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="react-micro-frontend">
+      <div className="App">
+        <header className="App-header">
+          <img src={'https://angularreactmicromicrofrontend.s3.amazonaws.com/karlface.jpg'} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
+    
   );
 }
 
